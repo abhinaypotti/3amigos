9feca510a0686c474a4f6f8b96c7645750ff09e1
vishalm1320 <65185070+vishalm1320@users.noreply.github.com>
2020-07-09 15:25:54 +0530

    Update tictactoe.cpp

--- C++ implementation/tictactoe.cpp
+++ C++ implementation/tictactoe.cpp
@@ -4,12 +4,12 @@
 using std::cout;
 using std::cin;
 
-int player, ai;
+int bestmov[2];
 
 bool isBoardFull(int arr[9]) {
 
 	for (int i = 0; i < 9; i++) {
-		if (arr[i] == '-')
+		if (arr[i] == Player::none)
 			return false;
 	}
 	return true;
@@ -23,30 +23,62 @@ int checkWin(int arr[9]) {
 		return -1;
 }
 
-bool gameOver(int arr[9]) {
-	
-	if (isBoardFull(arr))
-		return 2;
+/*bool gameOver(int arr[9]) {
 
-		return checkWin(arr);
-}
+		return checkWin(arr) || isBoardFull(arr);
+}*/
 
-int minimax(int arr[9], int depth, bool max) {
+int minimax(int arr[9], int depth, Player player) {
+
+	int score[2];
 
 	int test = checkWin(arr);
 
-	if (test == 10)
+	if (test == 1)
 		return test;
-	if (test == -10)
+	if (test == -1)
 		return test;
 
-	if (gameOver(arr))
+	if (isBoardFull(arr))
 		return 0;
 
-	if (max) {
-		int bestmov = -1000;
+	if (player = Player::human)
+		bestmov[1] = +1000;
+	else
+		bestmov[1] = -1000;
 
+	for (int i = 0; i < 9; i++) {
+		if (arr[i] == Player::none) {
+			arr[i] = player;
+			if (player == Player::ai) {
+				score[0] = i;
+				score[1] = minimax(arr, depth - 1, Player::human);
+			}
+			else {
+
+				score[0] = i;
+				score[1] = minimax(arr, depth - 1, Player::ai);
+			}
+			arr[i] = Player::none;
+
+			if (player == Player::ai) {
+				if (score[1] > bestmov[1]) {
+					bestmov[1] = score[1];
+					bestmov[0] = bestmov[0];
+				}
+			}
+			else
+				if (score[1] < bestmov[1]) {
+					bestmov[0] = score[0];
+					bestmov[1] - score[1];
+				}
+		}
 	}
+
+	return bestmov[1];
+
+
+
 }
 
 int main() {
