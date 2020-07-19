958f810e8047a06a3506885578c650ff74206740
gaautaam <62436952+gaautaam@users.noreply.github.com>
2020-07-19 14:08:25 +0530

    Update home_page.dart

--- Latest code/ui/home_page.dart
+++ Latest code/ui/home_page.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:project/ui/game_page.dart';
+import 'game_presenter.dart';
 
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+  
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  GamePresenter d;
+
   @override
   Widget build(BuildContext context) {
 
@@ -22,7 +26,18 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Text("Welcome to Flutter Tic Tac Toe!", style: TextStyle(fontSize: 20),),
+          Text("Welcome to Flutter Tic Tac Toe!", style: TextStyle(fontSize: 50),),
+          Text("Select the depth",style: TextStyle(fontSize: 50),),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 570, vertical: 10.0),
+          child: Row(children: [
+            
+            RaisedButton(onPressed: ()=>d.Depth(1),child: Text("1",style: TextStyle(fontSize: 50),),),
+            RaisedButton(onPressed: ()=>d.Depth(2),child: Text("2",style: TextStyle(fontSize: 50),),),
+            RaisedButton(onPressed: ()=>d.Depth(3),child: Text("3",style: TextStyle(fontSize: 50),),),
+            RaisedButton(onPressed: ()=>d.Depth(4),child: Text("Max",style: TextStyle(fontSize: 50),),),
+          ],),
+        ),
           Center(
             child: ButtonTheme(
               minWidth: 200,
@@ -30,7 +45,7 @@ class _HomePageState extends State<HomePage> {
               child: RaisedButton(
                 shape: RoundedRectangleBorder(
                   side: BorderSide(color: Colors.amber, width: 2),
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
                 ),
                 color: Colors.amber,
                 onPressed: () {
@@ -43,9 +58,10 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-
+       
         ],
       ),
     );
   }
 }
+
